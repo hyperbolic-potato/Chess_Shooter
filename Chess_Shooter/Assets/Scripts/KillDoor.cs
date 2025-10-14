9f393409e04202817d159c747806d1ef7be20719
hyperbolic-potato <duncan.kernaghan@outlook.com>
2025-10-14 13:45:24 -0700

    Added sword animations and started on the killdoor mechanic

--- Chess_Shooter/Assets/Scripts/KillDoor.cs
+++ Chess_Shooter/Assets/Scripts/KillDoor.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.AI;
+using Unity.AI.Navigation;
 
 public class KillDoor : MonoBehaviour
 {
+    public int enemies;
+    public int threshold = 1;
+    TextMeshPro tmp;
+    NavMeshSurface nms;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tmp = transform.GetChild(0).GetComponent<TextMeshPro>();
+        nms = GameObject.FindGameObjectWithTag("Navmesh").GetComponent<NavMeshSurface>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        tmp.text = (enemies - threshold).ToString();
         
+        if(enemies <= threshold)
+        {
+            nms.UpdateNavMesh(nms.navMeshData);
+            Destroy(gameObject);
+        }
     }
 }
--- Chess_Shooter/Assets/Scripts/Weapons/Sword.cs
+++ Chess_Shooter/Assets/Scripts/Weapons/Sword.cs
@@ -3,11 +3,15 @@ using System.Collections;
 public class Sword : Weapon
 {
     MeshRenderer mesh;
+
+    public float angleNoise = 5f;
+    public float angleSeverity = 45f;
     protected override void Start()
     {
         base.Start();
         mesh = transform.GetChild(1).GetComponent<MeshRenderer>();
     }
+
     public override void fire()
     {
         if (canFire && !reloading && (clip > 0 || clipSize == -1) && weaponID > -1)
@@ -17,9 +21,14 @@ public class Sword : Weapon
             GameObject p = Instantiate(projectile, firePoint.position, firePoint.rotation);
             p.transform.parent = transform;
 
-            
+            //me on my way to program a suphisticated, obscure, and unnecessary detail knowing full well noone is going to notice it:
 
-            p.transform.localRotation *= Quaternion.AngleAxis(Random.Range(45f, -45f), Vector3.forward);
+            p.transform.localRotation *= Quaternion.AngleAxis(Random.Range(angleNoise, -angleNoise), Vector3.forward);
+            p.transform.localRotation *= Quaternion.AngleAxis(Mathf.Clamp((transform.rotation.eulerAngles.x + 180) % 360, 180f - angleSeverity, 180 + angleSeverity) - 180, Vector3.forward);
+            
+            //waiter, waiter, more quaternion bullshittery please!
+
+            
             p.tag = "PlayerDamage";
             Destroy(p, projLifespan);
             if (clipSize != -1) clip--;
